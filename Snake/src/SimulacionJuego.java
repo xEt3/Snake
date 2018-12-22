07873229aed442134a987dd3c3182b8fb32c72a3
Nacho <belmonteperona@gmail.com>
2018-12-22 21:49:23 +0100

    añadido tiempo de espera cuando te matan

--- Snake/src/SimulacionJuego.java
+++ Snake/src/SimulacionJuego.java
@@ -61,6 +61,12 @@ public class SimulacionJuego extends GameOver {
 		actualizarCuerpo();
 
 		if (chocaConSerpiente()) {
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			mostrarPantallaGameOver(lvl);
 			ClasePrincipal.ventanaJuego.dispose();
 			ejecucion.stop();
